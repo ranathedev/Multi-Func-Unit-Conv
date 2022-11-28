5c75a9a25e1a13a435fd5b7c77869ded20e13d1e
ranaintizar <intizaralirana2@gmail.com>
2022-11-28 14:20:41 +0500

    Add Close on Click Away to Navbar Dropdown Menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,71 +1,89 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import DropdownArr from "../../assets/dropdown-arr.svg";
+
+import useClickOnOutside from "../../lib/hooks";
 
 import stl from "./Navbar.module.scss";
 
 const Navbar = () => {
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
+  const [isOpenCommon, setIsOpenCommon] = useState(true);
+  const [isOpenEngineer, setIsOpenEngineer] = useState(true);
+
+  const close1 = () => {
+    setIsOpenCommon(true);
+    const drp1 = document.getElementById("drpdwn1");
+    drp1.style.width = "0";
+    drp1.style.padding = "0";
+    drp1.style.height = "0";
+    drp1.style.opacity = "0";
+    console.log("Common-List-false");
+  };
+
+  const close2 = () => {
+    setIsOpenEngineer(true);
+    const drp2 = document.getElementById("drpdwn2");
+    drp2.style.width = "0";
+    drp2.style.padding = "0";
+    drp2.style.height = "0";
+    drp2.style.opacity = "0";
+    console.log("Engineer-List-false");
+  };
+
+  const common_ref = useRef();
+  const engineer_ref = useRef();
+
+  useClickOnOutside(close1, common_ref);
+  useClickOnOutside(close2, engineer_ref);
 
   return (
     <div className={stl.container}>
       <ul className={stl.mainList}>
         <li>Home</li>
         <li
+          ref={common_ref}
           id="common_conv"
           className={stl.common_conv}
           onClick={() => {
-            if (isOpen1) {
-              setIsOpen1(!isOpen1);
+            if (isOpenCommon) {
+              setIsOpenCommon(!isOpenCommon);
               const drp1 = document.getElementById("drpdwn1");
               drp1.style.width = "100%";
               drp1.style.padding = "1rem";
               drp1.style.height = "220px";
               drp1.style.opacity = "1";
-              console.log("true");
-            } else if (!isOpen1) {
-              setIsOpen1(true);
-              const drp1 = document.getElementById("drpdwn1");
-              drp1.style.width = "0";
-              drp1.style.padding = "0";
-              drp1.style.height = "0";
-              drp1.style.opacity = "0";
-              console.log("false");
+              console.log("Common-List-true");
+            } else if (!isOpenCommon) {
+              close1();
             }
           }}
         >
           Common Converters <DropdownArr />
-          <ul id="drpdwn1" className={stl.drownDown}>
+          <ul ref={common_ref} id="drpdwn1" className={stl.drownDown}>
             <li>example</li>
             <li>example</li>
             <li>example</li>
           </ul>
         </li>
         <li
+          ref={engineer_ref}
           id="engineer_conv"
           className={stl.engineer_conv}
           onClick={() => {
-            if (isOpen2) {
-              setIsOpen2(!isOpen2);
+            if (isOpenEngineer) {
+              setIsOpenEngineer(!isOpenEngineer);
               const drp2 = document.getElementById("drpdwn2");
               drp2.style.width = "100%";
               drp2.style.padding = "1rem";
               drp2.style.height = "220px";
               drp2.style.opacity = "1";
-              console.log("true");
-            } else if (!isOpen2) {
-              setIsOpen2(true);
-              const drp2 = document.getElementById("drpdwn2");
-              drp2.style.width = "0";
-              drp2.style.padding = "0";
-              drp2.style.height = "0";
-              drp2.style.opacity = "0";
-              console.log("false");
+              console.log("Engineer-List-true");
+            } else if (!isOpenEngineer) {
+              close2();
             }
           }}
         >
           Engineer Converters <DropdownArr />
-          <ul id="drpdwn2" className={stl.drownDown}>
+          <ul ref={engineer_ref} id="drpdwn2" className={stl.drownDown}>
             <li>example</li>
             <li>example</li>
             <li>example</li>
