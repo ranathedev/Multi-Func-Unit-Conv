7028750211f22099a326a05bfd6e529193853e6b
ranaintizar <intizaralirana2@gmail.com>
2022-12-01 00:52:28 +0500

    Add Conveter Data API

--- pages/stories/unit-converter.js
+++ pages/stories/unit-converter.js
@@ -1,115 +1,130 @@
+import { useEffect } from "react";
+import { getDocs } from "firebase/firestore";
+import { collectionRef } from "../api/firebase-config";
+
 import Stories from "../../src/components/stories";
 import UnitConverter from "../../src/components/unit-converter/Unit-Converter";
 
-const lengthConverter = {
-  arg: "Unit-Converter-Length",
-  type: "Length",
-  units: [
-    {
-      name: "Mile",
-      symbol: "mi",
-    },
-    {
-      name: "Kilometer",
-      symbol: "km",
-    },
-    {
-      name: "Meter",
-      symbol: "m",
-    },
-    {
-      name: "Foot",
-      symbol: "ft",
-    },
-    {
-      name: "Yard",
-      symbol: "yd",
-    },
-    {
-      name: "Inch",
-      symbol: "in",
-    },
-    {
-      name: "Hectometer",
-      symbol: "hm",
-    },
-    {
-      name: "Decameter",
-      symbol: "dam",
-    },
-    {
-      name: "Decimeter",
-      symbol: "dm",
-    },
-    {
-      name: "Milimeter",
-      symbol: "mm",
-    },
-    {
-      name: "Centimeter",
-      symbol: "dm",
-    },
-  ],
-};
+const UnitConverterStory = () => {
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const data = await getDocs(collectionRef);
+  //     console.log(data.docs);
+  //   };
 
-const timeConverter = {
-  arg: "Unit-Converter-Time",
-  type: "Time",
-  units: [
-    { name: "Milisecond", symbol: "ms" },
-    { name: "Second", symbol: "s" },
-    { name: "Minute", symbol: "min" },
-    { name: "Hour", symbol: "h" },
-    { name: "Day", symbol: "d" },
-    { name: "Week", symbol: "w" },
-    { name: "Month", symbol: "m" },
-    { name: "Year", symbol: "yr" },
-  ],
-};
+  //   getData();
+  // }, []);
 
-const AoSConverter = {
-  arg: "Unit-Converter-AmountofSubstance",
-  type: "AmountofSubstance",
-  units: [{ name: "mole", symbol: "mol" }],
-};
+  const lengthConverter = {
+    arg: "Unit-Converter-Length",
+    type: "Length",
+    units: [
+      {
+        name: "Mile",
+        symbol: "mi",
+      },
+      {
+        name: "Kilometer",
+        symbol: "km",
+      },
+      {
+        name: "Meter",
+        symbol: "m",
+      },
+      {
+        name: "Foot",
+        symbol: "ft",
+      },
+      {
+        name: "Yard",
+        symbol: "yd",
+      },
+      {
+        name: "Inch",
+        symbol: "in",
+      },
+      {
+        name: "Hectometer",
+        symbol: "hm",
+      },
+      {
+        name: "Decameter",
+        symbol: "dam",
+      },
+      {
+        name: "Decimeter",
+        symbol: "dm",
+      },
+      {
+        name: "Milimeter",
+        symbol: "mm",
+      },
+      {
+        name: "Centimeter",
+        symbol: "dm",
+      },
+    ],
+  };
 
-const e_CurrentConverter = {
-  arg: "Unit-Converter-ElectricCurrent",
-  type: "ElectricCurrent",
-  units: [
-    { name: "", symbol: "" },
-    { name: "", symbol: "" },
-    { name: "", symbol: "" },
-  ],
-};
+  const timeConverter = {
+    arg: "Unit-Converter-Time",
+    type: "Time",
+    units: [
+      { name: "Milisecond", symbol: "ms" },
+      { name: "Second", symbol: "s" },
+      { name: "Minute", symbol: "min" },
+      { name: "Hour", symbol: "h" },
+      { name: "Day", symbol: "d" },
+      { name: "Week", symbol: "w" },
+      { name: "Month", symbol: "m" },
+      { name: "Year", symbol: "yr" },
+    ],
+  };
 
-const tempratureConverter = {
-  arg: "Unit-Converter-Temprature",
-  type: "Temprature",
-};
-const l_IntensityConverter = {
-  arg: "Unit-Converter-LuminousIntensity",
-  type: "LuminousIntensity",
-};
+  const AoSConverter = {
+    arg: "Unit-Converter-AmountofSubstance",
+    type: "AmountofSubstance",
+    units: [{ name: "mole", symbol: "mol" }],
+  };
 
-const massConverter = {
-  arg: "Unit-Converter-Mass",
-  type: "Mass",
-};
+  const e_CurrentConverter = {
+    arg: "Unit-Converter-ElectricCurrent",
+    type: "ElectricCurrent",
+    units: [
+      { name: "", symbol: "" },
+      { name: "", symbol: "" },
+      { name: "", symbol: "" },
+    ],
+  };
 
-const UnitConverterStory = () => (
-  <Stories
-    argList={[
-      lengthConverter,
-      timeConverter,
-      AoSConverter,
-      e_CurrentConverter,
-      tempratureConverter,
-      l_IntensityConverter,
-      massConverter,
-    ]}
-    template={(args) => <UnitConverter {...args} />}
-  />
-);
+  const tempratureConverter = {
+    arg: "Unit-Converter-Temprature",
+    type: "Temprature",
+  };
+  const l_IntensityConverter = {
+    arg: "Unit-Converter-LuminousIntensity",
+    type: "LuminousIntensity",
+  };
+
+  const massConverter = {
+    arg: "Unit-Converter-Mass",
+    type: "Mass",
+  };
+
+  return (
+    <Stories
+      argList={[
+        lengthConverter,
+        timeConverter,
+        AoSConverter,
+        e_CurrentConverter,
+        tempratureConverter,
+        l_IntensityConverter,
+        massConverter,
+      ]}
+      template={(args) => <UnitConverter {...args} />}
+    />
+  );
+};
 
 export default UnitConverterStory;
