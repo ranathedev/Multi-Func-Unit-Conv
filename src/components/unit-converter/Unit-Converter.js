9e32a453f3bb8c53bf830fc07e8bf68c9daa3165
ranaintizar <intizaralirana2@gmail.com>
2022-12-02 00:07:13 +0500

    Add CloseonClickAway to Unit-Converter Dropdowns

--- src/components/unit-converter/Unit-Converter.js
+++ src/components/unit-converter/Unit-Converter.js
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+
+import useClickOnOutside from "../../lib/hooks";
 
 import DropdownIcon from "../../assets/dropdown-arr.svg";
 
 import stl from "./Unit-Converter.module.scss";
 
 const UnitConverter = ({ type, data }) => {
+  // const [ConvType, setConvType] = useState(type);
   const [inputValue, setInputValue] = useState("Select...");
   const [outputValue, setOutputValue] = useState("Select...");
   const [value, setValue] = useState(1);
+
+  // useEffect(() => {
+  //   setInputValue("Select...");
+  //   setOutputValue("Select...");
+  // }, [ConvType]);
+
+  const inputValueRef = useRef();
+  const outputValueRef = useRef();
 
   const openDropDownInput = () => {
     const dropMenu = document.getElementById("dropDownInput");
@@ -32,66 +43,265 @@ const UnitConverter = ({ type, data }) => {
     e.target.placeholder = "";
   };
 
+  useClickOnOutside(closeDropDownInput, inputValueRef);
+  useClickOnOutside(closeDropDownOutput, outputValueRef);
+
   return (
-    <div className={stl.container}>
-      <h1>{type}</h1>
-      <div className={stl.dataContain}>
-        <div className={stl.input_value}>
-          <div className={stl.select}>
-            <button onClick={openDropDownInput} className={stl.dropDownBtn}>
-              {inputValue} <DropdownIcon />
-            </button>
-            <ul id="dropDownInput" className={stl.dropDown_input}>
-              {data.map((unit, i) => {
-                return (
-                  <li
-                    key={i}
-                    onClick={() => {
-                      closeDropDownInput();
-                      setInputValue(unit.name);
-                    }}
-                  >
-                    {unit.name} <span>({unit.symbol})</span>
-                  </li>
-                );
-              })}
-            </ul>
+    <>
+      {/* <div className={stl.test}>
+        <ul>
+          <li
+            onClick={() => {
+              setConvType("acceleration");
+            }}
+          >
+            acceleration
+          </li>
+          <li
+            onClick={() => {
+              setConvType("angle");
+            }}
+          >
+            angle
+          </li>
+          <li
+            onClick={() => {
+              setConvType("apparentPower");
+            }}
+          >
+            apparentPower
+          </li>
+          <li
+            onClick={() => {
+              setConvType("area");
+            }}
+          >
+            area
+          </li>
+          <li
+            onClick={() => {
+              setConvType("charge");
+            }}
+          >
+            charge
+          </li>
+          <li
+            onClick={() => {
+              setConvType("digtal");
+            }}
+          >
+            digital
+          </li>
+          <li
+            onClick={() => {
+              setConvType("each");
+            }}
+          >
+            each
+          </li>
+          <li
+            onClick={() => {
+              setConvType("energy");
+            }}
+          >
+            energy
+          </li>
+          <li
+            onClick={() => {
+              setConvType("force");
+            }}
+          >
+            force
+          </li>
+          <li
+            onClick={() => {
+              setConvType("frequency");
+            }}
+          >
+            frequency
+          </li>
+          <li
+            onClick={() => {
+              setConvType("illuminance");
+            }}
+          >
+            illuminance
+          </li>
+          <li
+            onClick={() => {
+              setConvType("length");
+            }}
+          >
+            length
+          </li>
+          <li
+            onClick={() => {
+              setConvType("mass");
+            }}
+          >
+            mass
+          </li>
+          <li
+            onClick={() => {
+              setConvType("pace");
+            }}
+          >
+            pace
+          </li>
+          <li
+            onClick={() => {
+              setConvType("partsPer");
+            }}
+          >
+            partsPer
+          </li>
+          <li
+            onClick={() => {
+              setConvType("pieces");
+            }}
+          >
+            pieces
+          </li>
+          <li
+            onClick={() => {
+              setConvType("power");
+            }}
+          >
+            power
+          </li>
+          <li
+            onClick={() => {
+              setConvType("pressure");
+            }}
+          >
+            pressure
+          </li>
+          <li
+            onClick={() => {
+              setConvType("reactiveEnergy");
+            }}
+          >
+            reactiveEnergy
+          </li>
+          <li
+            onClick={() => {
+              setConvType("reactivePower");
+            }}
+          >
+            reactivePower
+          </li>
+          <li
+            onClick={() => {
+              setConvType("speed");
+            }}
+          >
+            speed
+          </li>
+          <li
+            onClick={() => {
+              setConvType("temperature");
+            }}
+          >
+            temperature
+          </li>
+          <li
+            onClick={() => {
+              setConvType("time");
+            }}
+          >
+            time
+          </li>
+          <li
+            onClick={() => {
+              setConvType("voltage");
+            }}
+          >
+            voltage
+          </li>
+          <li
+            onClick={() => {
+              setConvType("volume");
+            }}
+          >
+            volume
+          </li>
+          <li
+            onClick={() => {
+              setConvType("volumeFlowRate");
+            }}
+          >
+            volumeFlowRate
+          </li>
+        </ul>
+      </div> */}
+      <div className={stl.container}>
+        <h1>{type}</h1>
+        <div className={stl.dataContain}>
+          <div className={stl.input_value}>
+            <div className={stl.select}>
+              <button onClick={openDropDownInput} className={stl.dropDownBtn}>
+                {inputValue} <DropdownIcon />
+              </button>
+              <ul
+                ref={inputValueRef}
+                id="dropDownInput"
+                className={stl.dropDown_input}
+              >
+                {data.map((unit, i) => {
+                  return (
+                    <li
+                      key={i}
+                      onClick={() => {
+                        closeDropDownInput();
+                        setInputValue(unit.name);
+                      }}
+                    >
+                      {unit.name} <span>({unit.symbol})</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <input
+              type="number"
+              placeholder="Enter value to Convert..."
+              onChange={(e) => setValue(e.target.value)}
+              onFocus={(e) => removePlaceholder(e)}
+            />
           </div>
-          <input
-            type="number"
-            placeholder="Enter value to Convert..."
-            onChange={(e) => setValue(e.target.value)}
-            onFocus={(e) => removePlaceholder(e)}
-          />
+          <div className={stl.output_value}>
+            <div className={stl.select}>
+              <button onClick={openDropDownOutput} className={stl.dropDownBtn}>
+                {outputValue} <DropdownIcon />
+              </button>
+              <ul
+                ref={outputValueRef}
+                id="dropDownOutput"
+                className={stl.dropDown_output}
+              >
+                {data.map((unit, i) => {
+                  return (
+                    <li
+                      key={i}
+                      onClick={() => {
+                        closeDropDownOutput();
+                        setOutputValue(unit.name);
+                      }}
+                    >
+                      {unit.name} <span>({unit.symbol})</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <span className={stl.output}>0</span>
+          </div>
         </div>
-        <div className={stl.output_value}>
-          <div className={stl.select}>
-            <button onClick={openDropDownOutput} className={stl.dropDownBtn}>
-              {outputValue} <DropdownIcon />
-            </button>
-            <ul id="dropDownOutput" className={stl.dropDown_output}>
-              {data.map((unit, i) => {
-                return (
-                  <li
-                    key={i}
-                    onClick={() => {
-                      closeDropDownOutput();
-                      setOutputValue(unit.name);
-                    }}
-                  >
-                    {unit.name} <span>({unit.symbol})</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <span className={stl.output}>0</span>
+        <div className={stl.Btn}>
+          <button className={stl.convBtn}>Convert</button>
         </div>
       </div>
-      <div className={stl.Btn}>
-        <button className={stl.convBtn}>Convert</button>
-      </div>
-    </div>
+    </>
   );
 };
 
